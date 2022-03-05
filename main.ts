9f7b31d6261d3053f27ce1ad406748069a959879
evmichalenko <77173276+evmichalenko@users.noreply.github.com>
2022-03-05 11:22:36 -0500

    Input for timers added

--- main.ts
+++ main.ts
@@ -1,26 +1,47 @@
 input.onButtonPressed(Button.A, function () {
-    Start = true
+    SetUp = true
+    StartScreenMessage = false
+    basic.showNumber(_of_Tabatas)
 })
 input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
-    Start = false
+    Start = true
+    StartScreenMessage = false
+})
+input.onGesture(Gesture.Shake, function () {
+    if (SetUp == true) {
+        basic.showNumber(_of_Tabatas)
+        _of_Tabatas += 1
+    }
 })
 let Start = false
+let SetUp = false
+let _of_Tabatas = 0
+let StartScreenMessage = false
+StartScreenMessage = true
+_of_Tabatas = 1
 let Work = 10
 let Rest = 5
+while (StartScreenMessage == true) {
+    basic.showString("A for Setup B to start")
+}
 basic.forever(function () {
     if (Start == true) {
-        while (Work > 0) {
-            basic.showNumber(Work)
-            Work += -1
-            basic.pause(500)
-        }
-        while (Rest > 0) {
-            basic.showNumber(Rest)
-            Rest += -1
-            basic.pause(500)
+        for (let index = 0; index < _of_Tabatas; index++) {
+            while (Work > 0) {
+                basic.showNumber(Work)
+                Work += -1
+                basic.pause(500)
+            }
+            while (Rest > 0) {
+                basic.showNumber(Rest)
+                Rest += -1
+                basic.pause(500)
+            }
+            Work = 10
+            Rest = 5
         }
         control.reset()
     }
